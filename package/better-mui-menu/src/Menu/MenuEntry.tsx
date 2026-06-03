c848efed4296d356f3036a800f54da9614e0b842
Martin Conte Mac Donell <reflejo@gmail.com>
2026-06-03 09:11:33 -0700

    Add alignSelf param to renderMenuIcon

--- package/better-mui-menu/src/Menu/MenuEntry.tsx
+++ package/better-mui-menu/src/Menu/MenuEntry.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import type { ReactNode, CSSProperties } from 'react';
 import { forwardRef, isValidElement } from 'react';
 import type { MenuItemProps } from '@mui/material';
 import { MenuItem as MuiMenuItem, Typography } from '@mui/material';
@@ -11,12 +11,12 @@ type MenuEntryProps = Omit<MenuItemProps, 'children'> & {
   endIcon?: MenuIcon;
 };
 
-const renderMenuIcon = (icon?: MenuIcon) => {
+const renderMenuIcon = (icon?: MenuIcon, alignSelf: CSSProperties['alignSelf']) => {
   if (!icon) return null;
   if (isValidElement(icon)) return icon;
   const IconComponent = icon;
   return (
-    <span style={{ alignSelf: 'flex-start', height: '100%', alignItems: 'center', display: 'flex' }}>
+    <span style={{ alignSelf, height: '100%', alignItems: 'center', display: 'flex' }}>
       <IconComponent />
     </span>
   );
@@ -29,11 +29,11 @@ export const MenuEntry = forwardRef<HTMLLIElement, MenuEntryProps>(function Menu
   return (
     <MuiMenuItem ref={ref} {...muiMenuItemProps} onClick={onClick}>
       <MenuItemContent>
-        {renderMenuIcon(startIcon)}
+        {renderMenuIcon(startIcon, 'flex-start')}
         <Typography component='span' sx={{ flex: 1, fontFamily: 'inherit' }}>
           {label}
         </Typography>
-        {renderMenuIcon(endIcon)}
+        {renderMenuIcon(endIcon, 'center')}
       </MenuItemContent>
     </MuiMenuItem>
   );
